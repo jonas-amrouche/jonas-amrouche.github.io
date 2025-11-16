f1fcbc2d562df1e0ffaf1ebad5c495652859ffd4
Frizons <38401883+Frizons@users.noreply.github.com>
2025-11-16 17:37:09 +0100

    attempt to fix gltf import 2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-// vite.config.js
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "/jonas-amrouche.github.io/",
-});
+  base: '/jonas-amrouche.github.io/', // Replace with your actual repository name
+  build: {
+    outDir: 'dist',
+  }
+})
